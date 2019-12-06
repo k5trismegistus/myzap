03c09d999ada87440c8a9bba192cc9e8ea297e1c
keigo <k5.trismegistus@gmail.com>
2019-12-07 00:46:55 +0900

    :sparkles: Query task from algolia

--- fluuter_app/lib/models/myzap_task.dart
+++ fluuter_app/lib/models/myzap_task.dart
@@ -2,8 +2,8 @@ import 'package:myzap/models/myzap_situation.dart';
 
 class MyzapTask {
   final String id;
-  final String label;
+  final String description;
   final List<MyzapSituation> situations;
 
-  MyzapTask(this.id, this.label, this.situations);
+  MyzapTask(this.id, this.description, this.situations);
 }
--- fluuter_app/lib/pages/top.dart
+++ fluuter_app/lib/pages/top.dart
@@ -3,8 +3,7 @@ import 'package:myzap/layouts/defaultLayout.dart';
 import 'package:myzap/pages/addTaskPage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:myzap/utils/algolia.dart';
-
-// Below: for mock implementation
+import 'package:myzap/models/myzap_task.dart';
 
 class SelectableSituation {
   String objectId;
@@ -13,14 +12,6 @@ class SelectableSituation {
 
   SelectableSituation(this.objectId, this.label);
 }
-
-List<String> situations = [
-  'Before go to bed, 30 minutes',
-  'During commuting',
-  'Friday night',
-];
-
-// Above: for mock implementation
 
 class TopPage extends StatefulWidget {
   TopPage({Key key, this.title}) : super(key: key);
@@ -52,100 +43,133 @@ class _TopPageState extends State<TopPage> {
     var snapshot = await Firestore.instance.collection('situations').limit(10).getDocuments();
     var rst = snapshot.documents.map((doc) {
       return new SelectableSituation(
-        doc['objectID'],
+        doc.documentID,
         doc['label'],
       );
     }).toList();
     return rst;
   }
 
-  List<Widget> situationChips() {
-    return this._choices.map((choice) {
-      return InputChip(
-        label: Text(choice.label),
-        labelStyle: TextStyle(color: choice.selected ? Colors.black : Colors.grey, fontSize: 16),
-        onPressed: () {
-          setState(()  => choice.selected = !choice.selected);
-        }
-      );
-    }).toList();
-  }
+  Future<MyzapTask> queryTask() async {
+      var selectedSituationIds = this._choices.where((c) => c.selected).map((c) => c.objectId);
+      var _snap = await AlgoliaStore.getInstance().index('tasks').search(selectedSituationIds.join(', ')).getObjects();
 
-  void showAction() {
-    showDialog(
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          title: Text("Guru's oracle..."),
-          content: Text("Go to bed now"),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("No"),
-              onPressed: () => Navigator.pop(context),
-            ),
-            FlatButton(
-              child: Text("OK"),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ],
+      if (_snap.hits.length == 0) {
+        return null;
+      }
+
+      var selected = (_snap.hits..shuffle()).first;
+      return new MyzapTask(selected.objectID, selected.data['description'], []);
+    }
+
+    List<Widget> situationChips() {
+      return this._choices.map((choice) {
+        return InputChip(
+          label: Text(choice.label),
+          labelStyle: TextStyle(color: choice.selected ? Colors.black : Colors.grey, fontSize: 16),
+          onPressed: () {
+            setState(()  => choice.selected = !choice.selected);
+          }
         );
-      },
-    );
-  }
+      }).toList();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return DefaultLayout(
-      title: widget.title,
-      page: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Expanded(
-                  child: Wrap(
-                    alignment: WrapAlignment.start,
-                    spacing: 8.0,
-                    runSpacing: 0.0,
-                    direction: Axis.horizontal,
-                    children: this.situationChips()
+    void showAction() async {
+      var task = await this.queryTask();
+
+      if (task == null) {
+        showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text("Guru has no answer..."),
+              content: Text(''),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("OK"),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      }
+
+      showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text("Guru's oracle..."),
+            content: Text(task.description),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("No"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return DefaultLayout(
+        title: widget.title,
+        page: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Wrap(
+                      alignment: WrapAlignment.start,
+                      spacing: 8.0,
+                      runSpacing: 0.0,
+                      direction: Axis.horizontal,
+                      children: this.situationChips()
+                    )
                   )
-                )
-              ]
-            ),
-            FlatButton(
-              onPressed: this._loadChoices,
-              color: Colors.blue,
-              child: Text(
-                'Reload',
-                style: TextStyle(
-                  color:Colors.white,
-                  fontSize: 20.0
+                ]
+              ),
+              FlatButton(
+                onPressed: this._loadChoices,
+                color: Colors.blue,
+                child: Text(
+                  'Reload',
+                  style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 20.0
+                  ),
                 ),
               ),
-            ),
-            FlatButton(
-              onPressed: this.showAction,
-              color: Colors.blue,
-              child: Text(
-                'Tell me what to do',
-                style: TextStyle(
-                  color:Colors.white,
-                  fontSize: 20.0
+              FlatButton(
+                onPressed: this.showAction,
+                color: Colors.blue,
+                child: Text(
+                  'Tell me what to do',
+                  style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 20.0
+                  ),
                 ),
-              ),
-            )
-          ]
+              )
+            ]
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, '/addTask'),
-        tooltip: 'Add new Task!',
-        child: Icon(Icons.add),
-      ), //
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, '/addTask'),
+          tooltip: 'Add new Task!',
+          child: Icon(Icons.add),
+        ), //
+      );
+    }
   }
-}
