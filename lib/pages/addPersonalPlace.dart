185394c1bfc6929cec694b3a6a34c263f9ef8506
keigo <k5.trismegistus@gmail.com>
2020-01-20 22:40:03 +0900

    :sparkles: Move after search

--- lib/pages/addPersonalPlace.dart
+++ lib/pages/addPersonalPlace.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:myzap/layouts/defaultLayout.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import "package:google_maps_webservice/places.dart";
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 
@@ -14,6 +13,9 @@ class AddPersonalPlacePage extends StatefulWidget {
 class _AddPersonalPlacePageState extends State<AddPersonalPlacePage> {
 
   GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: DotEnv().env['GOOGLE_MAPS_API_KEY']);
+  LatLng _location = LatLng(35.681236,139.767125);
+
+  GoogleMapController _googleMapController;
 
   @override
   Widget build(BuildContext context) {
@@ -39,15 +41,23 @@ class _AddPersonalPlacePageState extends State<AddPersonalPlacePage> {
                 );
               },
               onSuggestionSelected: (PlacesSearchResult suggestion) {
-                print(suggestion.geometry.toJson());
+                var loc = LatLng(suggestion.geometry.location.lat, suggestion.geometry.location.lng);
+                this.setState(() {
+                  this._location = loc;
+                  this._googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+                    CameraPosition(target: loc, zoom: 17.0)
+                  ));
+                });
               }
             ),
             SizedBox(
               height: 200,
               child: GoogleMap(
-                onMapCreated: (GoogleMapController controller) {},
+                onMapCreated: (GoogleMapController controller) {
+                  this._googleMapController = controller;
+                },
                 initialCameraPosition: CameraPosition(
-                  target: LatLng(35.681236,139.767125),
+                  target: this._location,
                   zoom: 17.0,
                 ),
                 myLocationEnabled: true,
