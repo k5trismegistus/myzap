99c0627f776b49e9b740021e4f58fc53f3d221b4
keigo <k5.trismegistus@gmail.com>
2020-07-28 21:26:26 +0900

    :fire: Remove unused import

--- lib/pages/add_personal_place.dart
+++ lib/pages/add_personal_place.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
